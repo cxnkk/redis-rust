18610245ced53ebdbcc64e64dc828e267a53baa2
cxnkk <cenkoezkartal@gmail.com>
2026-01-30 10:08:32 +0100

    respond to multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 #![allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
-            }
+            Ok(mut stream) => loop {
+                let mut buffer = [0; 512];
+                let bytes_read = stream.read(&mut buffer);
+
+                match bytes_read {
+                    Ok(0) => break,
+                    Ok(_) => stream.write_all(b"+PONG\r\n").unwrap(),
+                    Err(_) => break,
+                }
+            },
             Err(e) => {
                 println!("error: {}", e);
             }
